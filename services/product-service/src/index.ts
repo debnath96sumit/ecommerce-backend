e2cc1f79dbbccee7783ff2c645a8f7383a685b96
sumit <sumit.debnath@webskitters.in>
2025-04-17 19:48:43 +0530

    Initial commit

---
+++ services/product-service/src/index.ts
@@ -0,0 +1,27 @@
+// src/index.ts
+import express from 'express';
+import dotenv from 'dotenv';
+import dbConnect from './config/dbConnect';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ?? 4001;
+
+app.use(express.json());
+
+// Health check
+app.get('/api/products/health', (req, res) => {
+  res.send({ message: 'Product service is healthy' });
+});
+
+// Connect to Mongo and start server
+const start = async () => {
+  await dbConnect();
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Product Service running on port ${PORT}`);
+  });
+};
+
+start();
